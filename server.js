fac1a6b0c890f3ca3df5a6d54dcd4a47ddb6b87b
rumeysaaerz <erizrumeyssa@gmail.com>
2026-01-01 13:47:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,25 +11,16 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-// ÖNEMLİ: Statik dosyalar her şeyden önce gelmeli// server.js içinde bu satırı tam olarak böyle yaz:
-// server.js içinde bu satırı tam olarak böyle güncelle:
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 
-// --- ROTALARIN DOĞRU BAĞLANMASI ---
-
-// Sayfalar (Login, Dashboard vb.) doğrudan ana dizinden çalışmalı
 app.use('/', pageRoutes); 
-
-// Giriş işlemleri
 app.use('/', authRoutes);
-
-// Sadece veri çeken API'ler /api altında kalabilir
 app.use('/api', kdsRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 112 AKILLI KDS Aktif: http://localhost:${PORT}`);
+    console.log(` 112 AKILLI KDS Aktif: http://localhost:${PORT}`);
 });
