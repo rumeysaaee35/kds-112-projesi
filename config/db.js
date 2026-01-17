0f8f3d8f384c76ddd13aa2e61bb354fe3c4b330f
rumeysaaerz <erizrumeyssa@gmail.com>
2026-01-17 22:51:12 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root', 
-    database: 'kds_projesi',
-    port: 8889
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
